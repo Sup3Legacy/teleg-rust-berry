bc679c84e98f21c5e9bf0eb66d62b321b7120bcb
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2020-12-24 00:04:07 +0100

    added `ping` test function

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+use futures::StreamExt;
+use telegram_bot::*;
+
+async fn pong(api : &Api, message : &Message) -> Result<(), Error> {
+    api.send(message.text_reply("pong")).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set :(");
+    let api = Api::new(token);
+
+    let mut stream = api.stream();
+    while let Some(update) = stream.next().await {
+        let update = update?;
+        if let UpdateKind::Message(message) = update.kind {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                println!("<{}>: {}", &message.from.first_name, data);
+                api.send(message.text_reply(format!(
+                    "Hi, {}! You just wrote '{}'",
+                    &message.from.first_name, data
+                )))
+                .await?;
+                if data == "/ping" {
+                    pong(&api, &message).await?;
+                }
+            }
+        }
+    }
+    Ok(())
+}
