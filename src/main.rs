42364f6f3c564b4d5e89fde746c6376e8d1c9d26
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2021-01-26 16:21:30 +0100

    added very basic JSON capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use std::env;
 use futures::StreamExt;
 use telegram_bot::*;
+use json;
+use std::io::Read;
+use serde::Deserialize;
+use serde_json;
 
 async fn pong(api : &Api, message : &Message) -> Result<(), Error> {
     api.send(message.text_reply("pong")).await?;
     Ok(())
-    
 }
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Person {
+    firsname : String,
+    lastname : String,
+    birthplace : String,
+    birthdate : String,
+    location : String,
+    signature : String
+}
+
+fn read_json_to_person() -> Person {
+    let mut value = String::new();
+    let mut file = std::fs::File::open("../../data.json").unwrap();
+    file.read_to_string(&mut value).unwrap();
+    serde_json::from_str(&value).expect("JSON file we incorrectly formatted")
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
